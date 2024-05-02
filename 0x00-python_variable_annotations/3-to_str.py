827f9651309e6b2f2748a435457f0a974bb97ab0
aissap <jesus.jesus.core@gmail.com>
2024-05-02 23:57:37 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Function that converts a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation."""
+    return str(n)
