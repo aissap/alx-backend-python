2cfef564b90ff54ee4782610c45e390b4bbf51f9
aissap <jesus.jesus.core@gmail.com>
2024-05-02 23:10:41 +0000

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+"""Function that concatenates two strings."""
 
-"""Function that concatenates two strings.""" 
 
 def concat(str1: str, str2: str) -> str:
     """return the result."""
