29e966b49a0baf3c2148354bf9c6126b12b99359
aissap <jesus.jesus.core@gmail.com>
2024-05-02 23:03:08 +0000

    update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 """a function that adds two floats.""" 
 def add(a: float, b: float) -> float:
-  """Return the sum."""
-  return a + b
+    """Return the sum."""
+    return a + b
