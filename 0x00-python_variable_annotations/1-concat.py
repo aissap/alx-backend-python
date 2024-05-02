833e69b9cae3ad2109457ee0ba2a661c45e7c4a8
aissap <jesus.jesus.core@gmail.com>
2024-05-02 23:53:46 +0000

    2-floor.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """Function that concatenates two strings."""
 
 
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Function that calculates the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float."""
+    return int(n)
