4db1893a84309379b8869dd22184ade68fb43228
aissap <jesus.jesus.core@gmail.com>
2024-05-03 00:30:28 +0000

    100-safe_first_element.py

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Function that retrieves the first element of a sequence."""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence) -> Union[Any, None]:
+    """Return the first element of lst or None."""
+    if lst:
+        return lst[0]
+    else:
+        return None
