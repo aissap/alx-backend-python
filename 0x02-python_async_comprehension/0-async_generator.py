d56b223f8923f013e0260bf84edc52d2ee9e026e
aissap <jesus.jesus.core@gmail.com>
2024-05-07 16:07:36 +0000

    0-async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 '''Async Generator Module.'''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''Generate a sequence of random numbers asynchronously.
 
     Generate a sequence of 10 random numbers between 0 and 10,
